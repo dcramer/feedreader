783d28ef54d4b896538c99b2b3f7b66240f0596b
David Cramer <dcramer@gmail.com>
2011-01-02 22:42:43 -0800

    Bump version to 0.3.1

--- feedreader/__init__.py
+++ feedreader/__init__.py
@@ -2,7 +2,7 @@ import os.path
 
 __all__ = ('__version__', '__build__')
 
-__version__ = (0, 3)
+__version__ = (0, 3, 1)
 
 def _get_git_revision(path):
     revision_file = os.path.join(path, 'refs', 'heads', 'master')
