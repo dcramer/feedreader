29316c08cac2cffcfd19598b227bd7fbf985c648
David Cramer <dcramer@gmail.com>
2011-03-25 15:21:42 -0700

    Test env support with unittest2

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ import feedreader
 
 setup(
     name='feedreader',
-    version=".".join(map(str, feedreader.__version__)),
+    version='0.3.1',
     author='David Cramer',
     author_email='dcramer@gmail.com',
-    url='http://github.com/dcramer/feedreader',
+    url='https://github.com/dcramer/feedreader',
     install_requires=[
         'lxml',
         'python-dateutil',
@@ -17,4 +17,6 @@ setup(
     description = 'A generic RSS/Atom feedparser.',
     packages=find_packages(),
     include_package_data=True,
+    tests_require=['unittest2'],
+    test_suite='unittest2.collector',
 )
